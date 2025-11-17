24e7e813154b7784b0ac649e9660595c0f2d6ceb
fevico <ajayifavour81@gmail.com>
2025-11-17 08:00:21 +0100

    is boooked status added to booking

---
+++ prisma/migrations/20251117065508_booking_status/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "apartment" ADD COLUMN     "isBooked" BOOLEAN NOT NULL DEFAULT false;
--- src/routes/banner.route.ts
+++ src/routes/banner.route.ts
@@ -10,7 +10,7 @@ router.patch("/:id", authenticateAdmin, updateBanner)
 router.delete("/:id", authenticateAdmin, deleteBanner)
 router.get("/:id", authenticateAdmin, getBannerById)
  
-export default router    
+export default router      
 
 // discount for property and this would remove the discount from the total amount if it is more than one days
 // generate dicount with code start and and end date with a particular appartment and the amount and this is a one time code 
